e64cc188c66d6210f4eedcb9f077d1c5866d6b58
davien21 <chidiebereekennia@gmail.com>
2020-10-16 19:00:53 +0100

    comment on initial requirements

---
+++ app.js
@@ -0,0 +1,9 @@
+
+// Install and import express
+// Create an HTTP Server
+// Export app for testing
+// Set up error logging
+// Set up routing
+// Set up startup folder
+// Set up config / env variables
+// Set up middleware: auth, admin, validate, etc.
