c4719263ea07eafb3aa8eb073b5ca11990e2d51b
davien21 <chidiebereekennia@gmail.com>
2020-10-16 21:00:56 +0100

    pass test to see if http server is running

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
+const winston = require('winston');
+const express = require('express');
+const app = express();
+const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.send({message:'success'})
+})
+
+app.use(express.json());
+app.use('/', router);
+
+
+const port = process.env.PORT || 3000;
+
+
+const server = app.listen(port, () => winston.info(`Server running at port ${port}`));
+
+module.exports = server;
 // Set up testing for app.js
 // Install and import express
 // Create an HTTP Server
