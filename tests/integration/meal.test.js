c8e7f9ca7991b92fd0364b7c158bb043f0dcb50f
davien21 <chidiebereekennia@gmail.com>
2020-10-17 21:41:35 +0100

    add test to check if admin is logged in

--- tests/integration/meal.test.js
+++ tests/integration/meal.test.js
@@ -23,33 +23,52 @@ describe('/api/meals', () => {
       expect(res.status).toBe(401)
     })
     it(('should return 400 if name is falsy'), async () => {
+      token = new Admin().generateAuthToken();
+      
       const res = await request(server)
         .post('/api/meals')
+        .set('x-auth-token', token)
         .send()
+
       expect(res.status).toBe(400)
     })
     it(('should return 400 if day is falsy'), async () => {
+      token = new Admin().generateAuthToken();
+      
       const res = await request(server)
         .post('/api/meals')
+        .set('x-auth-token', token)
         .send({name: 'chidi'})
+
       expect(res.status).toBe(400)
     })
     it(('should return 400 if day is not a valid day of the week'), async () => {
+      token = new Admin().generateAuthToken();
+      
       const res = await request(server)
         .post('/api/meals')
+        .set('x-auth-token', token)
         .send({name: 'chidi', day: 'omo'})
+
       expect(res.status).toBe(400)
     })
     it(('should return 200 if inputs are valid'), async () => {
+      token = new Admin().generateAuthToken();
+      
       const res = await request(server)
         .post('/api/meals')
+        .set('x-auth-token', token)
         .send({name: 'chidi', day: 'monday'})
+
       expect(res.status).toBe(200)
     })
     it(('should save meal if inputs are valid'), async () => {
+      token = new Admin().generateAuthToken();
+
       const mealId = mongoose.Types.ObjectId()
       const res = await request(server)
         .post('/api/meals')
+        .set('x-auth-token', token)
         .send({_id: mealId,name: 'chidi', day: 'monday'})
   
       const meal = await Meal.findById(mealId)
