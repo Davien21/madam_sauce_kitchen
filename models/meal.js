13f5dd994078a513a8b53bd54c468dac8489df37
davien21 <chidiebereekennia@gmail.com>
2020-10-17 23:46:22 +0100

    add validation logic on meal with joi and add static method for looking up meals

--- models/meal.js
+++ models/meal.js
@@ -12,8 +12,25 @@ model.mealSchema = new mongoose.Schema({
     type: String,
   }
 })
-
+model.mealSchema.statics.lookup = function(name, day) {
+  return this.findOne({
+    "name": name,
+    "day": day
+  })
+}
 model.Meal = mongoose.model('Meal', model.mealSchema)
+
+model.validateMeal = (meal) => {
+  const schema = {
+    name: Joi.string().min(5).max(50).required(),
+    day: Joi.string()
+    .valid(
+      'monday', 'tuesday', 'wednesday', 
+      'thursday', 'friday', 'saturday', 'sunday'
+    ).required().insensitive()
+  }
+	return result = Joi.validate(meal,schema);
+} 
 
 module.exports = model;
 
