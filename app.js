9ab37eac6fab68f7ee6e4aa45889ff22b1bd130a
davien21 <chidiebereekennia@gmail.com>
2020-10-18 00:04:15 +0100

    refactor db logic and require / import instead

--- app.js
+++ app.js
@@ -6,33 +6,21 @@ const app = express();
 
 const mongoose = require('mongoose');
 
-const db = config.get('db')
-mongoose.connect(db,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-})
-.then(() => winston.info(`Connected to ${db} ...`))
+require('./startup/db')();
 
 const { Meal, validateMeal } = require('./models/meal')
 
 const auth = require('./middleware/auth')
+const validateBody = require('./middleware/validateBody')
 
 const meals = express.Router();
 
-meals.post('/', auth, async (req, res) => {
-  const { error }  = validateMeal(req.body);
-	if (error) return res.status(400).send(error.details[0].message);
-
-  let mealInDB = await Meal.lookup(req.body.name, req.body.day)
+meals.post('/', [auth, validateBody(validateMeal)], async (req, res) => {
+  const mealInDB = await Meal.lookup(req.body.name, req.body.day)
 
   if(mealInDB) return res.status(400).send('This Meal already exists')
 
-  let meal = new Meal({
-    name: req.body.name,
-    day: req.body.day
-  })
+  let meal = new Meal({ name: req.body.name, day: req.body.day })
   meal = await meal.save()
 
   res.send(meal);
