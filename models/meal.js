17f2bb731b485c8e2683dadbf29af998a3345209
davien21 <chidiebereekennia@gmail.com>
2020-10-17 21:16:25 +0100

    create meal model

---
+++ models/meal.js
@@ -0,0 +1,21 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+let model = {};
+model.mealSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength : 5, 
+		maxlength : 50,
+  },
+  day: {
+    type: String,
+  }
+})
+
+model.Meal = mongoose.model('Meal', model.mealSchema)
+
+ 
+
+module.exports = model;
+
