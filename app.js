26d3aef86d1ebf07d340931bd4e4ad9fac2f1b13
davien21 <chidiebereekennia@gmail.com>
2020-10-17 19:47:39 +0100

    pass test to return 404 if name of the meal  is falsy

--- app.js
+++ app.js
@@ -6,15 +6,20 @@ const app = express();
 const mongoose = require('mongoose');
 
 const db = config.get('db')
-mongoose.connect(db,
-  { useNewUrlParser: true, useUnifiedTopology: true})
-  .then(() => winston.info(`Connected to ${db} ...`))
+mongoose.connect(db,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+.then(() => winston.info(`Connected to ${db} ...`))
 
 const { Meal } = require('./models/meal')
 const meals = express.Router();
 
 meals.post('/api/meals', async (req, res) => {
-  res.send({message:'success'})
+  if (!req.body.name) return res.status(400).send('Invalid meal')
+ 
 })
 
 app.use(express.json());
