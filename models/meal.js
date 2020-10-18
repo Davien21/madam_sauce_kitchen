8902525b1444febeccc67446cc45543b57b973bb
davien21 <chidiebereekennia@gmail.com>
2020-10-18 14:46:30 +0100

    add code to pass sort by days function test

--- models/meal.js
+++ models/meal.js
@@ -12,12 +12,21 @@ model.mealSchema = new mongoose.Schema({
     type: String,
   }
 })
+
 model.mealSchema.statics.lookup = function(name, day) {
   return this.findOne({
     "name": name,
     "day": day
   })
 }
+
+model.sortByDays = (mealArray) => {
+  order = { sunday: 1, monday: 2, tuesday: 3, wednesday: 4, thursday: 5, friday: 6, saturday: 7 };
+  return mealArray.sort(function (a, b) {
+    return order[a.day] - order[b.day];
+  });
+}
+
 model.Meal = mongoose.model('Meal', model.mealSchema)
 
 model.validateMeal = (meal) => {
