194aa2ccbe676b0abfc396567daa74a7d7169892
davien21 <chidiebereekennia@gmail.com>
2020-10-18 22:40:02 +0100

    move winston error handling logic to logging.js in startup folder

--- app.js
+++ app.js
@@ -4,16 +4,8 @@ const app = express();
 
 require('express-async-errors');
 
-winston.handleExceptions(
-  new winston.transports.Console({ colorize: true, prettyPrint: true }),
-  new winston.transports.File({ filename: 'uncaughtExceptions.log'}))
- 
-process.on('unhandledRejection', (ex)=> {
-  throw ex; 
-})
-winston.add(winston.transports.File, { filename: 'logfile.log' });
-
 require('./startup/db')();
+require('./startup/logging')();
 
 const meals = require('./routes/meals')
 const error = require('./middleware/error')
