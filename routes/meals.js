c6bc9d1c34aafdfbd3f77a295f0903bc31e50e70
davien21 <chidiebereekennia@gmail.com>
2020-10-18 14:49:22 +0100

    add code to pass test to return all meals with a get request

--- routes/meals.js
+++ routes/meals.js
@@ -1,20 +1,25 @@
-const { Meal, validateMeal } = require('../models/meal')
+const { Meal, validateMeal, sortByDays } = require('../models/meal')
 const auth = require('../middleware/auth')
 const validateBody = require('../middleware/validateBody')
 const express = require('express');
 const router = express.Router();
+
+router.get('/', async (req, res) => {
+  let meal = await Meal.find().sort('name')
+  meal = sortByDays(meal)
+	res.send(meal);
+})
 
 router.post('/', [auth, validateBody(validateMeal)], async (req, res) => {
   const mealInDB = await Meal.lookup(req.body.name, req.body.day)
 
   if(mealInDB) return res.status(400).send('This Meal already exists')
 
-  let meal = new Meal({ name: req.body.name, day: req.body.day })
+  let meal = new Meal({ name: req.body.name, day: req.body.day.toLowerCase() })
   meal = await meal.save()
 
   res.send(meal);
 })
-
 
 
 module.exports = router;
