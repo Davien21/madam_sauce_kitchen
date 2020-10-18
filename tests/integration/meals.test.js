94141291f98801f58998ea02e34f101c6e36fb10
davien21 <chidiebereekennia@gmail.com>
2020-10-18 14:48:42 +0100

    rename to meals.test because it's testing for the meals router and add test check if get request returns all meals

---
+++ tests/integration/meals.test.js
@@ -0,0 +1,111 @@
+const { Meal } = require('../../models/meal')
+const { Admin } = require('../../models/admin')
+const request = require('supertest');
+const mongoose = require('mongoose')
+
+describe('/api/meals', () => {
+  let server = require('../../app')
+  beforeEach( async () => { await Meal.deleteMany({}) })
+  
+  afterEach( async () => {
+    await server.close(); 
+    await Meal.deleteMany({})
+    
+  })
+ 
+  describe('GET /', () => {
+    it(('should return all available meals'), async () => {
+      await Meal.collection.insertMany([
+        { name: 'banga soup', day: 'friday' },
+        { name: 'jellof rice', day: 'tuesday' },
+      ])
+
+      const res = await request(server).get('/api/meals')
+      
+      expect(res.status).toBe(200)
+      expect(res.body.length).toBe(2);
+      expect(res.body.some(g => g.name === 'banga soup')).toBeTruthy()
+      expect(res.body.some(g => g.day === 'friday')).toBeTruthy()
+      expect(res.body.some(g => g.name === 'jellof rice')).toBeTruthy()
+      expect(res.body.some(g => g.day === 'tuesday')).toBeTruthy()
+    
+    })
+   })
+
+  describe('POST /', () => {
+    let token;
+    let name;
+    let day;
+    let meal;
+
+    beforeEach( () => {
+      
+      name = 'chidi'
+      day = 'monday'
+      meal = new Meal({
+        name,
+        day
+      })
+      token = new Admin().generateAuthToken();
+    })
+    const exec = () => {
+      return request(server)
+        .post('/api/meals')
+        .set('x-auth-token', token)
+        .send({ name, day})
+    }
+    it(('should return 401 if not logged in'), async () => {
+
+      token = "";
+
+      const res = await exec();
+
+      expect(res.status).toBe(401)
+    })
+    it(('should return 400 if name is falsy'), async () => {
+      name = "";
+
+      const res = await exec();
+
+      expect(res.status).toBe(400);
+    })
+    it(('should return 400 if day is falsy'), async () => {
+      day = "";
+      
+      const res = await exec();
+
+      expect(res.status).toBe(400)
+    })
+    it(('should return 400 if day is not a valid day of the week'), async () => {
+      day = 'bad day'
+      
+      const res = await exec();
+
+      expect(res.status).toBe(400)
+    })
+    it(('should return 400 if meal is already in DB'), async () => {
+      await meal.save();
+
+      const res = await exec();
+
+      expect(res.status).toBe(400)
+    })
+    it(('should return 200 if inputs are valid'), async () => {
+      const res = await exec();
+
+      expect(res.status).toBe(200)
+    })
+    it(('should save meal if inputs are valid'), async () => {
+      await exec();
+  
+      const mealInDB = await Meal.lookup(name, day)
+
+      expect(mealInDB).not.toBeNull()
+      expect(mealInDB).toMatchObject({name, day})
+    })
+  })
+
+})
+// Remaining tests:
+// return 400 if name is less than 5 characters
+// return 400 if name is more than 50 characters
