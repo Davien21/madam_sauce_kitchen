746c7eb65821a111c715bc14c7953476dd1c5ecc
davien21 <chidiebereekennia@gmail.com>
2020-10-18 20:58:49 +0100

    add code to turn all days in body of request to lowercase

--- middleware/validateBody.js
+++ middleware/validateBody.js
@@ -5,6 +5,8 @@ module.exports = validate = (validator) => {
   return (req, res, next) => {
     const { error } = validator(req.body);
     if (error) return res.status(400).send(error.details[0].message);
+
+    if (req.body.day) req.body.day = req.body.day.toLowerCase()
     next();
   }
 }
