375dfaad61be9916a81a2dde1c83ce5a79484721
davien21 <chidiebereekennia@gmail.com>
2020-10-18 20:59:24 +0100

    change status code that is returned to 404

--- middleware/validateObjectId.js
+++ middleware/validateObjectId.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = function (req, res, next) {
   if (!mongoose.Types.ObjectId.isValid(req.params.id))
-    return res.status(400).send('Invalid Id');
+    return res.status(404).send('Invalid Id');
     
   next();
   
