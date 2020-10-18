dd9d32192052d47d75819057e46358d8043e3c4e
davien21 <chidiebereekennia@gmail.com>
2020-10-18 22:42:14 +0100

    add all winston error handling logic

---
+++ startup/logging.js
@@ -0,0 +1,18 @@
+require('express-async-errors');
+const winston = require('winston');
+// require('winston-mongodb'); // - if you want to log stuff to db
+
+module.exports = function() {
+  winston.handleExceptions(
+    new winston.transports.Console({ colorize: true, prettyPrint: true }),
+    new winston.transports.File({ filename: 'uncaughtExceptions.log'}))
+    
+    process.on('unhandledRejection', (ex)=> {
+    throw ex; 
+  })
+  winston.add(winston.transports.File, { filename: 'logfile.log' });
+  // winston.add(winston.transports.MongoDB, { 
+  //   db: 'mongodb://localhost/vidly',
+  //   level: 'info'
+  // });
+}
