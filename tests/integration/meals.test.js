1f454567f99fe06e8ac406d080dd723eaf956dfa
davien21 <chidiebereekennia@gmail.com>
2020-10-19 10:59:43 +0100

    include a price in the request body and test for all routes

--- tests/integration/meals.test.js
+++ tests/integration/meals.test.js
@@ -16,8 +16,8 @@ describe('/api/meals', () => {
   describe('GET /', () => {
     it(('should return all available meals'), async () => {
       await Meal.collection.insertMany([
-        { name: 'banga soup', day: 'friday' },
-        { name: 'jellof rice', day: 'tuesday' },
+        { name: 'banga soup', day: 'friday', price: 3400 },
+        { name: 'jellof rice', day: 'tuesday', price: 2100 },
       ])
 
       const res = await request(server).get('/api/meals')
@@ -27,6 +27,7 @@ describe('/api/meals', () => {
       res.body.forEach((meal) => {
         expect(meal).toHaveProperty('name', meal.name)
         expect(meal).toHaveProperty('day', meal.day)
+        expect(meal).toHaveProperty('price', meal.price)
       })
     })
   })
@@ -39,10 +40,11 @@ describe('/api/meals', () => {
     })
    
     it('should return meals for that day if input is valid', async () => {
+
       await Meal.collection.insertMany([
-        { name: 'banga soup', day: 'friday' },
-        { name: 'jellof rice', day: 'tuesday' },
-        { name: 'ofe akwu', day: 'tuesday' },
+        { name: 'banga soup', day: 'friday', price: 7500 },
+        { name: 'jellof rice', day: 'tuesday', price: 8500 },
+        { name: 'ofe akwu', day: 'tuesday', price: 9500 },
       ])
 
       const res = await request(server).get('/api/meals/?day=tuesday');
@@ -52,6 +54,7 @@ describe('/api/meals', () => {
       res.body.forEach((meal) => {
         expect(meal).toHaveProperty('name', meal.name)
         expect(meal).toHaveProperty('day', meal.day)
+        expect(meal).toHaveProperty('price', meal.price)
       })
     
     })
@@ -62,7 +65,7 @@ describe('/api/meals', () => {
     let mealId;
 
     beforeEach( async () => {
-      meal = new Meal({ name: 'chidi', day: 'sunday' })
+      meal = new Meal({ name: 'chidi', day: 'sunday', price: 7500 })
       mealId = meal._id
       await meal.save()
     })
@@ -71,6 +74,7 @@ describe('/api/meals', () => {
       return request(server)
       .get('/api/meals/' + mealId);
     }
+    
     it('should return 404 if id is invalid', async () => {
       mealId = 1;
 
@@ -78,6 +82,7 @@ describe('/api/meals', () => {
 
       expect(res.status).toBe(404);
     })
+    
     it('should return 404 if meal with given id does not exist', async () => {
       mealId = mongoose.Types.ObjectId();
 
@@ -85,15 +90,17 @@ describe('/api/meals', () => {
 
       expect(res.status).toBe(404);
     })
+    
     it('should return 200 if meal with given id exists', async () => {
       const res = await exec();
 
       expect(res.status).toBe(200);
     })
+    
     it('should return meal in body of the response if id is valid', async () => {
       const res = await exec();
 
-      expect(res.body).toMatchObject({name: 'chidi', day: 'sunday'});
+      expect(res.body).toMatchObject({name: 'chidi', day: 'sunday', price: 7500});
     })
    
   })
@@ -103,20 +110,23 @@ describe('/api/meals', () => {
     let name;
     let day;
     let meal;
+    let price;
 
     beforeEach( () => {
-      
       name = 'chidi'
       day = 'monday'
-      meal = new Meal({ name, day })
+      price = 7500
+      meal = new Meal({ name, day, price })
       token = new Admin().generateAuthToken();
     })
+
     const exec = () => {
       return request(server)
         .post('/api/meals')
         .set('x-auth-token', token)
-        .send({ name, day})
+        .send({ name, day, price})
     }
+
     it(('should return 401 if not logged in'), async () => {
       token = "";
 
@@ -124,6 +134,7 @@ describe('/api/meals', () => {
 
       expect(res.status).toBe(401)
     })
+    
     it('should return 400 if token is invalid', async () => {
       token = 'a'; 
   
@@ -131,6 +142,7 @@ describe('/api/meals', () => {
   
       expect(res.status).toBe(400);
     });
+
     it(('should return 400 if name is falsy'), async () => {
       name = "";
 
@@ -138,6 +150,7 @@ describe('/api/meals', () => {
 
       expect(res.status).toBe(400);
     })
+
     it(('should return 400 if day is falsy'), async () => {
       day = "";
       
@@ -145,6 +158,7 @@ describe('/api/meals', () => {
 
       expect(res.status).toBe(400)
     })
+
     it(('should return 400 if day is not a valid day of the week'), async () => {
       day = 'bad day'
       
@@ -152,6 +166,23 @@ describe('/api/meals', () => {
 
       expect(res.status).toBe(400)
     })
+
+    it(('should return 400 if price is falsy'), async () => {
+      price = "";
+      
+      const res = await exec();
+
+      expect(res.status).toBe(400)
+    })
+
+    it(('should return 400 if price is not a valid number'), async () => {
+      price = "egoInWords";
+      
+      const res = await exec();
+
+      expect(res.status).toBe(400)
+    })
+
     it(('should return 400 if meal is already in DB'), async () => {
       await meal.save();
 
@@ -159,18 +190,20 @@ describe('/api/meals', () => {
 
       expect(res.status).toBe(400)
     })
+
     it(('should return 200 if inputs are valid'), async () => {
       const res = await exec();
 
       expect(res.status).toBe(200)
     })
+
     it(('should save meal if inputs are valid'), async () => {
       await exec();
   
       const mealInDB = await Meal.lookup(name, day)
 
       expect(mealInDB).not.toBeNull()
-      expect(mealInDB).toMatchObject({name, day})
+      expect(mealInDB).toMatchObject({name, day, price})
     })
   })
   
@@ -180,24 +213,27 @@ describe('/api/meals', () => {
     let newName;
     let newDay;
     let meal;
+    let newPrice;
 
     beforeEach( async () => {
       // Before each test we need to create a meal and 
       // put it in the database.
-      meal = new Meal({ name: 'chidi', day: 'monday' })
+      meal = new Meal({ name: 'chidi', day: 'monday', price: 1000 })
       await meal.save();
       
       token = new Admin().generateAuthToken();
       id = meal._id
       newName = 'chin-chin'
       newDay = 'sunday'
+      newPrice = 5000
     })
     const exec = () => {
       return request(server)
         .put('/api/meals/' + id)
         .set('x-auth-token', token)
-        .send({ name: newName, day: newDay})
+        .send({ name: newName, day: newDay, price: newPrice})
     }
+
     it(('should return 401 if not logged in'), async () => {
       token = "";
 
@@ -205,6 +241,7 @@ describe('/api/meals', () => {
 
       expect(res.status).toBe(401)
     })
+
     it(('should return 404 if id is invalid'), async () => {
       id = ''
 
@@ -212,6 +249,7 @@ describe('/api/meals', () => {
 
       expect(res.status).toBe(404)
     })
+
     it(('should return 404 if meal with given id is not found'), async () => {
       id = mongoose.Types.ObjectId()
 
@@ -219,6 +257,7 @@ describe('/api/meals', () => {
 
       expect(res.status).toBe(404)
     })
+
     it(('should return 400 if name is falsy'), async () => {
       newName = "";
 
@@ -226,6 +265,7 @@ describe('/api/meals', () => {
 
       expect(res.status).toBe(400);
     })
+
     it(('should return 400 if day is falsy'), async () => {
       newDay = "";
       
@@ -233,6 +273,7 @@ describe('/api/meals', () => {
 
       expect(res.status).toBe(400)
     })
+
     it(('should return 400 if day is not a valid day of the week'), async () => {
       newDay = 'bad day'
       
@@ -240,20 +281,40 @@ describe('/api/meals', () => {
 
       expect(res.status).toBe(400)
     })
+
+    it(('should return 400 if price is falsy'), async () => {
+      newDay = "";
+      
+      const res = await exec();
+
+      expect(res.status).toBe(400)
+    })
+
+    it(('should return 400 if price is not a number'), async () => {
+      newPrice = "moneyNoGoFinish";
+      
+      const res = await exec();
+
+      expect(res.status).toBe(400)
+    })
+
+
     it(('should update meal if inputs are valid'), async () => {
       await exec();
       
       const mealInDB = await Meal.lookup(newName, newDay)
       
       expect(mealInDB).not.toBeNull()
-      expect(mealInDB).toMatchObject({name: newName, day: newDay})
+      expect(mealInDB).toMatchObject({name: newName, day: newDay, price: newPrice})
     })
+
     it(('should return meal after update'), async () => {
       const res = await exec();
 
       expect(res.body).toHaveProperty('_id');
       expect(res.body).toHaveProperty('name', newName);
       expect(res.body).toHaveProperty('day', newDay);
+      expect(res.body).toHaveProperty('price', newPrice);
     
     })
   
@@ -274,12 +335,13 @@ describe('/api/meals', () => {
     beforeEach( async () => {
       // Before each test we need to create a meal and 
       // put it in the database.
-      meal = new Meal({ name: 'meal1', day: 'sunday' });
+      meal = new Meal({ name: 'meal1', day: 'sunday', price: 4000 });
       await meal.save();
 
       token = new Admin().generateAuthToken();     
       id = meal._id; 
     })
+    
     it('should return 401 if client is not logged in', async () => {
       token = '';
 
@@ -290,12 +352,14 @@ describe('/api/meals', () => {
    
 
     it('should return 404 for invalid id', async () => {
+
       id = 1;
 
       const res = await exec();
 
       expect(res.status).toBe(404);
     })
+    
     it('should return 404 if id is valid but meal does not exist', async () => {
       id = mongoose.Types.ObjectId();
 
@@ -304,6 +368,7 @@ describe('/api/meals', () => {
       expect(res.status).toBe(404);
     })
      
+    
     it('should remove meal if id is valid and meal exists', async () => {
       await exec();
       
@@ -311,12 +376,14 @@ describe('/api/meals', () => {
 
       expect(mealInDB).toBeNull();
     })
+    
     it('should return the removed meal if input is valid', async () => {
       const res = await exec();
       
       expect(res.body).toHaveProperty('_id');
       expect(res.body).toHaveProperty('name', 'meal1');
       expect(res.body).toHaveProperty('day', 'sunday');
+      expect(res.body).toHaveProperty('price', 4000);
     })
   })
 
