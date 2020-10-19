7fb1a3857a6767861a0efd36f3834dfc6f0e9fda
davien21 <chidiebereekennia@gmail.com>
2020-10-19 06:21:25 +0100

    add auth route for logins and authentication

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ require('./startup/logging')();
 
 const meals = require('./routes/meals')
 const admins = require('./routes/admins')
+const auth = require('./routes/auth')
+
 const error = require('./middleware/error')
 
 app.use(express.json());
 app.use('/api/meals', meals) // use the meals router;
 app.use('/api/admins', admins) // use the admins router;
+app.use('/api/auth', auth) // use the auth router;
  
 //error middleware
 app.use(error);
