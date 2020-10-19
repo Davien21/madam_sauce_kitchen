bee7ba472770c7c16f57dd47c207ba23b128f537
davien21 <chidiebereekennia@gmail.com>
2020-10-19 11:01:41 +0100

    pass refactored tests that include the price in request body

--- routes/meals.js
+++ routes/meals.js
@@ -1,5 +1,5 @@
 const { Meal, validateMeal, sortByDays } = require('../models/meal')
-
+const _ = require('lodash')
 const auth = require('../middleware/auth')
 const validateBody = require('../middleware/validateBody')
 const validateDay = require('../middleware/validateDay')
@@ -11,15 +11,15 @@ const router = express.Router();
 router.get('/', validateDay, async (req, res) => {
   let meals;
   if (req.query.day) 
-    meals = await Meal.find({ day: req.query.day}).sort('name').select('name day')
+    meals = await Meal.find({ day: req.query.day}).sort('name').select('name day price')
   
-  if (!req.query.day) meals = await Meal.find().sort('name').select('name day')
+  if (!req.query.day) meals = await Meal.find().sort('name').select('name day price')
  
 	res.send( sortByDays(meals) );
 })
 
 router.get('/:id', validateObjectId, async (req, res) => {
-  const meal = await Meal.findById(req.params.id).select('name day')
+  const meal = await Meal.findById(req.params.id).select('name day price')
 
 	if (!meal) return res.status(404).send('Invalid Meal')
 
@@ -31,16 +31,16 @@ router.post('/', [auth, validateBody(validateMeal)], async (req, res) => {
 
   if(mealInDB) return res.status(400).send('This Meal already exists')
 
-  let meal = new Meal({ name: req.body.name, day: req.body.day.toLowerCase() })
+  let meal = new Meal({ name: req.body.name, day: req.body.day.toLowerCase(), 
+    price: req.body.price })
   meal = await meal.save()
 
   res.send(meal);
 })
 
 router.put('/:id', [auth, validateObjectId, validateBody(validateMeal)], async (req, res) => {
-  const meal = await Meal.findByIdAndUpdate(req.params.id, {
-    name: req.body.name, day: req.body.day
-  }, { new: true })
+  const meal = await Meal.findByIdAndUpdate(req.params.id,
+    _.pick(req.body,['name', 'day', 'price']), { new: true })
 
   if(!meal) return res.status(404).send('Invalid Meal')
 
