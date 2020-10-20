bfcf60f1ad69862721b7ede22e46a104e4d4664e
davien21 <chidiebereekennia@gmail.com>
2020-10-20 05:40:18 +0100

    fix response message for invalid admin

--- routes/admins.js
+++ routes/admins.js
@@ -17,7 +17,7 @@ router.get('/', async (req,res) => {
 
 router.get('/:id', validateObjectId, async (req,res) => {
 	const admin = await Admin.findOne({ _id : req.params.id }).select('name')
-	if (!admin) return res.status(404).send('Admin'); 
+	if (!admin) return res.status(404).send('Invalid Admin'); 
 	res.send(admin);
 })
 
