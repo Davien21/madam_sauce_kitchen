d59ed09e08206cd70ff59467339381d0b935d15d
davien21 <chidiebereekennia@gmail.com>
2020-10-20 04:14:45 +0100

    add orders router and joi object id startup file

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ require('express-async-errors');
 
 require('./startup/db')();
 require('./startup/logging')();
-
+require('./startup/joi-objectId-validation')();
+ 
+const orders = require('./routes/orders')
 const meals = require('./routes/meals')
 const admins = require('./routes/admins')
 const auth = require('./routes/auth')
@@ -14,6 +16,7 @@ const auth = require('./routes/auth')
 const error = require('./middleware/error')
 
 app.use(express.json());
+app.use('/api/orders', orders) // use the orders router;
 app.use('/api/meals', meals) // use the meals router;
 app.use('/api/admins', admins) // use the admins router;
 app.use('/api/auth', auth) // use the auth router;
