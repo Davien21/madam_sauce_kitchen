d64d85b0d4a14524746c75349c34eee8070d45e5
davien21 <chidiebereekennia@gmail.com>
2020-10-20 04:24:13 +0100

    clean up and refactor the app file

--- app.js
+++ app.js
@@ -2,38 +2,13 @@ const winston = require('winston');
 const express = require('express');
 const app = express();
 
-require('express-async-errors');
-
-require('./startup/db')();
 require('./startup/logging')();
+require('./startup/routes')(app);
+require('./startup/db')();
 require('./startup/joi-objectId-validation')();
  
-const orders = require('./routes/orders')
-const meals = require('./routes/meals')
-const admins = require('./routes/admins')
-const auth = require('./routes/auth')
-
-const error = require('./middleware/error')
-
-app.use(express.json());
-app.use('/api/orders', orders) // use the orders router;
-app.use('/api/meals', meals) // use the meals router;
-app.use('/api/admins', admins) // use the admins router;
-app.use('/api/auth', auth) // use the auth router;
- 
-//error middleware
-app.use(error);
-
 const port = process.env.PORT || 3000;
-
 
 const server = app.listen(port, () => winston.info(`Server running at port ${port}`));
 
 module.exports = server;
-
-
-
-
-
-
-// Set up middleware: auth, admin, validate, etc.
