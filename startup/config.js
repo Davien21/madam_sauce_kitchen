5d0b98211efa39a824c7423c41fa1ab7265f64e0
davien21 <chidiebereekennia@gmail.com>
2020-10-20 14:42:13 +0100

    create config startup file to handle config env variable errors

---
+++ startup/config.js
@@ -0,0 +1,9 @@
+const winston = require('winston');
+const config = require('config');
+
+module.exports = function() {
+  if (!config.get('jwtPrivateKey')) {
+    throw new Error('FATAL ERROR: jwtPrivateKey is not defined.')
+  }
+}
+
