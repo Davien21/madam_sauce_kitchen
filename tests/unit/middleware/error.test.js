38573fe7a5fe2db9f93d58319a0115224615e3c7
davien21 <chidiebereekennia@gmail.com>
2020-10-21 11:49:09 +0100

    add test for error middleware

---
+++ tests/unit/middleware/error.test.js
@@ -0,0 +1,31 @@
+const error = require('../../../middleware/error')
+
+describe('error middleware', () => {
+  it('should return 500', () => {
+    const winston = {
+      error: jest.fn( (message, err) => {
+        console.log('omo')
+      })
+    }
+    
+    const err = {
+      message: jest.fn().mockReturnValue( )
+    }
+    const res = {
+      status: jest.fn( (statusCode) => {
+        
+      }).mockReturnValue({
+        send: jest.fn()
+      })
+    }
+
+    const req = {};
+    const next = jest.fn();
+
+    error(winston)(err, req, res, next)
+    
+    expect(winston.error).toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalled();
+    expect(res.status().send).toHaveBeenCalled();
+  })
+})
