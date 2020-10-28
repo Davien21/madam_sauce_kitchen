815bedd2a4fb56e67dc0ca2dbc6ebe308885260b
davien21 <chidiebereekennia@gmail.com>
2020-10-29 00:24:41 +0100

    cleaning up spaces and line breaks

--- middleware/validateBody.js
+++ middleware/validateBody.js
@@ -1,4 +1,4 @@
-// This function takes in a validator function and
+// This function takes in a joi validator function and
 // uses it to validate the body of a request
 
 module.exports = validate = (validator) => {
@@ -7,6 +7,7 @@ module.exports = validate = (validator) => {
     if (error) return res.status(400).send(error.details[0].message);
 
     if (req.body.day) req.body.day = req.body.day.toLowerCase()
+    
     next();
   }
 }
